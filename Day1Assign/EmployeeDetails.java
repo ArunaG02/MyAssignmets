737bf349c5e27c0b25b528fa3517fb347299f792
ArunaG02 <aruna04022003@gmail.com>
2025-12-06 21:30:56 +0530

    Add files via upload
    AssignmentDay1

---
+++ Day1Assign/EmployeeDetails.java
@@ -0,0 +1,29 @@
+package Day1Assign;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		String name = "Aruna Devi.G";
+		short age = 22;
+		int salary = 80000;
+		char gender = 'F';
+		boolean termsAndCondition = true ;
+		long phno = 95768990123L;
+		float rating = 9.9f;
+		int pincode = 600037;
+		String city = "Chennai";
+		short luckyno = 3;
+		System.out.println("NAME: "+ name);
+		System.out.println("AGE: "+ age);
+		System.out.println("SALARY: "+ salary);
+		System.out.println("GENDER: "+ gender);
+		System.out.println("TERMS AND CONDITION: "+ termsAndCondition);
+		System.out.println("PHONE NO: "+ phno);
+		System.out.println("PIN CODE: "+ pincode);
+		System.out.println("CITY: "+ city);
+		System.out.println("LUCKY NO: "+ luckyno);
+
+
+	}
+
+}
